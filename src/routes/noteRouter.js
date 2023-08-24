c33140c3c330506187832e901b23d3bc850dcd7a
osama maree <osamamarei2001@gmail.com>
2023-08-24 10:01:12 +0300

    Add Routes Pagination Query

--- src/routes/noteRouter.js
+++ src/routes/noteRouter.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { AddNote, DeleteNote, RetrieveNotes, UpdateNote } from "../controller/noteController.js";
+import { AddNote, DeleteNote, RetrieveNotes, RetrieveNotesWithPagination, UpdateNote } from "../controller/noteController.js";
 const router = Router();
 router.get("/", RetrieveNotes);
 router.post("/", AddNote);
 router.delete("/:id", DeleteNote);
 router.put("/:id", UpdateNote);
+router.get("/pagination", RetrieveNotesWithPagination);
 export default router;
